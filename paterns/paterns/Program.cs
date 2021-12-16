77aaec2c5bc4f4316525bccefe67f72b3d58216b
SirBlackwood <92998113+SirBlackwood@users.noreply.github.com>
2021-12-17 00:57:23 +0300

    Add files via upload

---
+++ paterns/paterns/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Text;
+
+namespace paterns
+{
+    interface ITransport
+    {
+        class CarColour
+        {
+            public string Colour { get; set; }
+        }
+        class BodyType
+        {
+            public string Body { get; set; }
+        }
+        class NumberOfDoors
+        {
+            public string Doors { get; set; }
+        }
+
+        abstract class Builder
+        {
+            public Car Car { get; set; }
+            public void CreateCar()
+            {
+                Car = new Car();
+            }
+            public abstract void SetColour();
+            public abstract void SetBody();
+            public abstract void SetNumberOfDoors();
+        }
+        class Concern
+        {
+            public Car Create(Builder builder)
+            {
+                builder.CreateCar();
+                builder.SetColour();
+                builder.SetBody();
+                builder.SetNumberOfDoors();
+                return builder.Car;
+            }
+        }
+        class SedanBuilder : Builder
+        {
+            public override void SetColour()
+            {
+                this.Car.CarColour = new CarColour { Colour = "Красный" };
+            }
+            public override void SetBody()
+            {
+                this.Car.BodyType = new BodyType { Body = "Седан" };
+            }
+            public override void SetNumberOfDoors()
+            {
+                this.Car.NumberOfDoors = new NumberOfDoors { Doors = "2" };
+            }
+        }
+        class HatchbackBuilder : Builder
+        {
+            public override void SetColour()
+            {
+                this.Car.CarColour = new CarColour { Colour = "Черный" };
+            }
+            public override void SetBody()
+            {
+                this.Car.BodyType = new BodyType { Body = "Хэтчбэк" };
+            }
+            public override void SetNumberOfDoors()
+            {
+                this.Car.NumberOfDoors = new NumberOfDoors { Doors = "4" };
+            }
+        }
+        class Car
+        {
+            public CarColour CarColour { get; set; }
+            public BodyType BodyType { get; set; }
+            public NumberOfDoors NumberOfDoors { get; set; }
+            public override string ToString()
+            {
+                StringBuilder sb = new StringBuilder();
+                if (BodyType != null)
+                    sb.Append("Тип кузова: " + BodyType.Body + "\n");
+                if (CarColour != null)
+                    sb.Append("Цвет: " + CarColour.Colour + "\n");
+                if (NumberOfDoors != null)
+                    sb.Append("Кол-во дверей: " + NumberOfDoors.Doors + "\n");
+                return sb.ToString();
+            }
+        }
+        void Drive();
+    }
+    class Auto : ITransport
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Машина едет по дороге \n");
+        }
+    }
+    class Driver
+    {
+        public void Travel(ITransport transport)
+        {
+            transport.Drive();
+        }
+    }
+    interface IAnimal
+    {
+        class CamelBuilder : ITransport.Builder
+        {
+            public override void SetColour()
+            {
+                this.Car.CarColour = new ITransport.CarColour { Colour = "Коричневый" };
+            }
+            public override void SetBody()
+            {
+                this.Car.BodyType = new ITransport.BodyType { Body = "Двугорбый" };
+            }
+            public override void SetNumberOfDoors()
+            {
+                this.Car.NumberOfDoors = new ITransport.NumberOfDoors { Doors = "0" };
+            }
+        }
+        void Move();
+    }
+    class Camel : IAnimal
+    {
+        public void Move()
+        {
+            Console.WriteLine("Верблюд идет по путыне \n");
+        }
+    }
+    class CamelToTransportAdapter : ITransport
+    {
+        Camel camel;
+        public CamelToTransportAdapter(Camel c)
+        {
+            camel = c;
+        }
+        public void Drive()
+        {
+            Console.WriteLine("Смена машины на верблюда");
+            camel.Move();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ITransport.Concern concern = new ITransport.Concern();
+
+            ITransport.Builder builder = new ITransport.SedanBuilder();
+            ITransport.Car sedan = concern.Create(builder);
+            Console.WriteLine(sedan.ToString());
+
+            builder = new ITransport.HatchbackBuilder();
+            ITransport.Car hatchback = concern.Create(builder);
+            Console.WriteLine(hatchback.ToString());
+
+            Driver driver = new Driver();
+            Auto auto = new Auto();
+            driver.Travel(auto);
+
+            Camel camel = new Camel();
+            ITransport camelTransport = new CamelToTransportAdapter(camel);
+
+            driver.Travel(camelTransport);
+
+            builder = new IAnimal.CamelBuilder();
+            ITransport.Car cam = concern.Create(builder);
+            Console.WriteLine(cam.ToString());
+
+
+            Console.Read();
+        }
+    }
+}
---
+++ paterns2/paterns2/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+namespace paterns2
+{
+    interface IMovable
+    {
+        void Move();
+    }
+    class Atack : IMovable
+    {
+        public void Move() { Console.WriteLine("Атакует"); }
+    }
+    class Defence : IMovable
+    {
+        public void Move() { Console.WriteLine("Обороняется"); }
+    }
+
+    class Checker
+    {
+        public void Check(Army army)
+        {
+            IUnitIter iter = army.CreateNum();
+            while (iter.HasNext())
+            {
+                Unit unit = iter.Next();
+                unit.GetName();
+            }
+        }
+    }
+
+    interface IUnitIter
+    {
+        bool HasNext();
+        Unit Next();
+    }
+    interface IUnitNum
+    {
+        IUnitIter CreateNum();
+        int Count { get; }
+        Unit this[int index] { get; }
+    }
+    class Unit
+    {
+        protected string name;
+        public void GetName() { Console.WriteLine(name); }
+        public Unit(string name, IMovable mov)
+        {
+            this.name = name;
+            Movable = mov;
+        }
+        public IMovable Movable { private get; set; }
+        public void Move()
+        {
+            Movable.Move();
+        }
+    }
+    class Army : IUnitNum
+    {
+        private Unit[] units;
+        public Army(Unit[] _units)
+        {
+            units = _units;
+        }
+        public int Count
+        {
+            get { return units.Length; }
+        }
+        public Unit this[int index]
+        {
+            get { return units[index]; }
+        }
+        public IUnitIter CreateNum()
+        {
+            return new ArmyNum(this);
+        }
+    }
+
+    class ArmyNum : IUnitIter
+    {
+        IUnitNum aggr;
+        int index = 0;
+        public ArmyNum(IUnitNum a)
+        {
+            aggr = a;
+        }
+        public bool HasNext()
+        {
+            return index < aggr.Count;
+        }
+        public Unit Next()
+        {
+            return aggr[index++];
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Unit unit = new Unit("Взвод 1", new Defence());
+            unit.Move();
+            unit.Movable = new Atack();
+            unit.Move();
+
+            Unit[] units = new Unit[]
+            {
+                unit,
+                new Unit("Взвод 2", new Atack()),
+                new Unit("Взвод 3", new Defence()),
+                new Unit("Взвод 4", new Atack()),
+                new Unit("Взвод 5", new Defence())
+            };
+
+            Army army = new Army(units);
+            Checker checker = new Checker();
+            checker.Check(army);
+        }
+    }
+}
